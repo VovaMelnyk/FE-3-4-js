a98870525cc5bf53780b725b4bfa5669223a3a1b
Vova Melnyk <hagmen4339@gmail.com>
2018-08-09 21:33:42 +0300

    MVC

---
+++ webpack-4-class/src/index.js
@@ -0,0 +1,7 @@
+import './scss/main.scss';
+import { add } from './js/sum';
+import {reverseString as rev} from './js/revers';
+console.log("hello, world");
+console.log(add(5,10));
+console.log(rev('qwe'));
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/import
